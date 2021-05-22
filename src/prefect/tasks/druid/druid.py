66612d5f1fc5e39c37e2d052846f25b925cb6920
Tenzin Choedak <tchoedak@gmail.com>
2021-05-22 11:29:06 -0700

    create initial tasks for druid queries

---
+++ src/prefect/tasks/druid/druid.py
@@ -0,0 +1,272 @@
+from typing import Union, List
+from pydruid.client import PyDruid
+from pydruid import db
+
+from prefect import Task
+from prefect.utilities.tasks import defaults_from_attrs
+
+
+class DruidExecute(Task):
+	"""
+	"""
+
+	def __init__(
+		self,
+		query: str = None,
+		host: str = 'localhost',
+		port: int = 8082,
+		path: str = '/druid/v2/sql/',
+		scheme: str = 'http',
+		user: str = None,
+		password: str = None,
+		context: str = None,
+		ssl_verify_cert: bool = True,
+		ssl_client_cert: str = None,
+		**kwargs
+	):
+		self.host = host
+		self.port = port
+		self.path = path
+		self.scheme = scheme
+		self.user = user
+		self.password = password
+		self.context = context
+		self.ssl_veirfy_cert = ssl_verify_cert
+		self.ssl_client_cert = ssl_client_cert
+		super().__init__(**kwargs)
+
+	@defaults_from_attrs(
+		"query",
+		"host",
+		"port",
+		"path",
+		"scheme",
+		"user",
+		"password",
+		"context",
+		"ssl_verify_cert",
+		"ssl_client_cert",
+	)
+	def run(
+		self,
+		query: str = None,
+		host: str = 'localhost',
+		port: int = 8082,
+		path: str = '/druid/v2/sql/',
+		scheme: str = 'http',
+		user: str = None,
+		password: str = None,
+		context: str = None,
+		ssl_verify_cert: bool = True,
+		ssl_client_cert: str = None,
+	):
+
+		if not query:
+			raise ValueError("A query string must be provided")
+
+		conn = db.connect(
+			host=host,
+			port=port,
+			path=path,
+			scheme=scheme,
+			user=user,
+			password=password,
+			context=context,
+			ssl_verify_cert=ssl_verify_cert,
+			ssl_client_cert=ssl_client_cert,
+		)
+
+		try:
+			with conn, cursor() as cursor:
+				executed = cursor.execute(query)
+				return executed
+
+		finally:
+			conn.close()
+
+
+class DruidTimeSeries(Task):
+	"""
+	"""
+	def __init__(
+		self,
+		url: str = 'http://localhost:8082',
+		endpoint: str = 'druid/v2',
+		cafile: str = None,
+		datasource: str = None,
+		granularity: str = None,
+		intervals: Union[str, List[str]] = None,
+		aggregations: dict = None,
+		**kwargs,
+	):
+		self.url = url
+		self.endpoint = endpoint
+		self.cafile = cafile
+		self.datasource = datasource
+		self.granularity = granularity
+		self.intervals = intervals
+		self.aggregations = aggregations
+		super().__init__(**kwargs)
+
+	@defaults_from_attrs(
+		"url",
+		"endpoint",
+		"cafile",
+		"datasource",
+		"granularity",
+		"intervals",
+		"aggregations",
+	)
+	def run(
+		self,
+		url: str = 'http://localhost:8082',
+		endpoint: str = 'druid/v2',
+		cafile: str = None,
+		datasource: str = None,
+		granularity: str = None,
+		intervals: Union[str, List[str]] = None,
+		aggregations: dict = None,
+		**druid_kwargs,
+	):
+		query_client = PyDruid(url=url, endpoint=endpoint, cafile=cafile)
+
+		time_series = query_client.time_series(
+			datasource=datasource,
+			granularity=granularity,
+			intervals=intervals,
+			aggregations=aggregations,
+			**druid_kwargs
+		)
+		return time_series
+
+
+class DruidTopN(Task):
+	"""
+	"""
+	def __init__(
+		self,
+		url: str = 'http://localhost:8082',
+		endpoint: str = 'druid/v2',
+		cafile: str = None,
+		datasource: str = None,
+		granularity: str = None,
+		intervals: Union[str, List[str]] = None,
+		aggregations: dict = None,
+		dimension: str = None,
+		metric: str = None,
+		threshold: int = None,
+		**kwargs,
+	):
+		self.url = url
+		self.endpoint = endpoint
+		self.cafile = cafile
+		self.datasource = datasource
+		self.granularity = granularity
+		self.intervals = intervals
+		self.aggregations = aggregations
+		self.dimension = dimension,
+		self.metric = metric
+		self.threshold = threshold
+		super().__init__(**kwargs)
+
+	@defaults_from_attrs(
+		"url",
+		"endpoint",
+		"cafile",
+		"datasource",
+		"granularity",
+		"intervals",
+		"aggregations",
+		"dimension",
+		"metric",
+		"threshold",
+	)
+	def run(
+		self,
+		url: str = 'http://localhost:8082',
+		endpoint: str = 'druid/v2',
+		cafile: str = None,
+		datasource: str = None,
+		granularity: str = None,
+		intervals: Union[str, List[str]] = None,
+		aggregations: dict = None,
+		dimension: str = None,
+		metric: str = None,
+		threshold: int = None,
+		**druid_kwargs,
+	):
+		query_client = PyDruid(url, endpoint, cafile)
+
+		top_n = query_client.topn(
+			datasource=datasource,
+			granularity=granularity,
+			intervals=intervals,
+			aggregations=aggregations,
+			dimension=dimension,
+			metric=metric,
+			threshold=threshold,
+			**druid_kwargs,
+		)
+
+
+		return top_n
+
+class DruidGroupBy(Task):
+	"""
+	"""
+	def __init__(
+		self,
+		url: str = 'http://localhost:8082',
+		endpoint: str = 'druid/v2',
+		cafile: str = None,
+		datasource: str = None,
+		granularity: str = None,
+		intervals: Union[str, List[str]] = None,
+		aggregations: dict = None,
+		dimensions: str = None,
+		**kwargs,
+	):
+		self.url = url
+		self.endpoint = endpoint
+		self.cafile = cafile
+		self.datasource = datasource
+		self.granularity = granularity
+		self.intervals = intervals
+		self.aggregations = aggregations
+		self.dimensions = dimensions
+		super().__init__(**kwargs)
+
+	@defaults_from_attrs(
+		"url",
+		"endpoint",
+		"cafile",
+		"datasource",
+		"granularity",
+		"intervals",
+		"aggregations",
+		"dimensions",
+	)
+	def run(
+		self,
+		url: str = 'http://localhost:8082',
+		endpoint: str = 'druid/v2',
+		cafile: str = None,
+		datasource: str = None,
+		granularity: str = None,
+		intervals: Union[str, List[str]] = None,
+		aggregations: dict = None,
+		dimensions: str = None,
+		**druid_kwargs,
+	):
+		query_client = PyDruid(url, endpoint, cafile)
+
+		group_by = query_client.groupby(
+			datasource=datasource,
+			granularity=granularity,
+			intervals=intervals,
+			aggregations=aggregations,
+			dimensions=dimensions,
+			**druid_kwargs,
+		)
+
+		return group_by
